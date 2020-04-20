16f39810db5a1f2443cb345c9d465d2cd9206b99
iamkaleb <kalebdmoran@gmail.com>
2020-04-20 16:00:51 -0500

    Added journal entries as objects

---
+++ src/scripts/journal.js
@@ -0,0 +1,35 @@
+const journalEntry = {
+    date: "",
+    conceptsCovered: "",
+    journalEntry: "",
+    mood: ""
+}
+
+const journalEntry1 = {
+    date: "4/20",
+    conceptsCovered: ["Functions", "Debugger"],
+    journalEntry: "Lectures on both basic JavaScript functions and how to use the debugger in dev tools. Got a lot of practice creating functions. Since there were separate exercises building off one initial directory, I also practiced copying directories and creating new git repositories.",
+    mood: "Productive"
+}
+
+const journalPages = [];
+
+journalPages.push(journalEntry1);
+
+const journalEntry2 = {
+    date: "4/17",
+    conceptsCovered:["Objects", "Manipulting DOM"],
+    journalEntry: "Lectures on the basics of JavaScript objects as well as how JavaScript can be used to modify the DOM.", 
+    mood: "Tired"
+}
+
+journalPages.push(journalEntry2);
+
+const journalEntry3 = {
+    date: "4/16",
+    conceptsCovered: ["Group project presentations", "Intro to JavaScript"],
+    journalEntry: "Presented our webpages from our first group projects. I had a great time working with my team to put together a cohesive website. We did a lot of pair programming. Everyone took turns solving problems for the groupa nd contrinuting to the overall design. We were then introduced to some basics of JavaScript, including the power it has to manipulate the DOM and modify what the viewer sees when the interact with a website.",
+    mood: "Appreciative"
+}
+
+journalPages.push(journalEntry3);
