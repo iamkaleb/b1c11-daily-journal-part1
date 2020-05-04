087b698c397e943e6c12d53124070bfe7fd1139f
iamkaleb <kalebdmoran@gmail.com>
2020-05-04 13:07:00 -0500

    Daily Journal 6

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,10 +1,12 @@
-// DEPENDENCIES: none
-
 // FUNCTION CREATING THE HTML STRUCTURE OF MY JOURNAL ENTRY
-function makeEntryComponent (entries) {
+const COMP = {
+  makeEntryComponent (entries) {
     return `
     <h2>${entries.conceptsCovered}</h2>
     <h3>${entries.date}</h3>
     <p>${entries.entry}</p>
     `
   }
+}
+
+  export default COMP
